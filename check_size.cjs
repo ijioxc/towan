5fa87da90ca4d2da042c6833e2581e87b1ff655d
ijioxc <ijioxc@gmail.com>
2026-07-01 20:05:34 +0800

    Organize root directory: move scripts and temp files to subfolders

--- check_size.cjs
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const sizeOf = require('image-size');
-const dimensions = sizeOf('/Users/ijioxc/.gemini/antigravity-ide/brain/f5d53d23-792f-45e7-be52-4e697684405b/towanworks_screenshot.png');
-console.log(dimensions.width, dimensions.height);
