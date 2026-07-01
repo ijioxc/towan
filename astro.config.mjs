005f7dc6fd34d509ff9ce71986b9b845d13f141d
ijioxc <ijioxc@gmail.com>
2026-07-01 09:58:19 +0800

    Add GitHub Pages deployment workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://ijioxc.github.io',
+  base: 'towan',
   vite: {
     plugins: [tailwindcss()]
   }
